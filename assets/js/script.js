b17257e1dcc0be6c8d2a333de32430297c3a2d7c
junnaedi <muhammad_junaidi@live.com>
2018-10-07 23:49:56 +0700

    First Portofolio. just text only

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+var options = {
+    strings: ["IT Support", "Server Administrator", "Programmer"],
+    typeSpeed: 80,
+    smartBackspace: true // Default value
+  }
+  
+  var typed = new Typed(".element", options);
