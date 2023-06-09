f4a5af427a24d52b63c68a9834f885bcc3cb1392
Junnaedi <muhammad_junaidi@live.com>
2023-06-09 12:59:29 +0700

    fix base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/junnaedi.github.io',
+  base: 'https://junnaedi.github.io',
   plugins: [react()],
 })
