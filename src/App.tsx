e6744292c88c8cafb112a426cf14b7b53ffdea94
Junnaedi <muhammad_junaidi@live.com>
2023-06-09 19:12:01 +0700

    added about me

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import { AnimatePresence, motion } from 'framer-motion';
+import { useTranslation } from 'react-i18next';
+import { FaCaretDown, FaChevronDown, FaEnvelope, FaPhoneAlt } from 'react-icons/fa';
 import './App.css'
+import './i18n';
+
 import blobBg from './assets/blob-haikei.svg';
+import profileImg from './assets/profile.png';
+import lineDivier from './assets/line.svg';
 
 function App() {
+  const { t, i18n } = useTranslation();
+
   return (
-    <div className={`bg-center bg-cover bg-no-repeat min-h-screen`} style={{ backgroundImage: `url(${blobBg})` }}>
+    <div className={`bg-top bg-cover bg-no-repeat min-h-screen`} style={{ backgroundImage: `url(${blobBg})` }}>
       <header className="">
         <div className="flex justify-between items-center mx-auto max-w-[1280px]">
           <div className="flex-1 p-5">
@@ -21,41 +29,46 @@ function App() {
             <ul className="flex justify-center items-center space-x-14">
               <li>
                 <a href="#" className="text-md font-bold text-[#344e41] hover:text-[#5b8c64] transition-all">
-                  Home
+                  {t('home')}
                 </a>
               </li>
               <li>
                 <a href="#" className="text-md font-bold text-[#344e41] hover:text-[#5b8c64] transition-all">
-                  About
+                  {t('about')}
                 </a>
               </li>
               <li>
                 <a href="#" className="text-md font-bold text-[#344e41] hover:text-[#5b8c64] transition-all">
-                  Projects
+                  {t('projects')}
                 </a>
               </li>
               <li>
                 <a href="#" className="text-md font-bold text-[#344e41] hover:text-[#5b8c64] transition-all">
-                  Support
+                  {t('support')}
                 </a>
               </li>
             </ul>
           </div>
           <div className="flex-1 inline-flex justify-end items-center">
             <div className="ml-auto">
-              <button className="px-6 py-3 bg-[#344e41] text-white rounded-pill font-bold text-sm hover:bg-[#3a5a40] transition-all">
-                Contact Me
+              <button onClick={() => {
+                i18n.changeLanguage(i18n.language === 'en' ? 'id' : 'en')
+              }} className="text-md p-0 bg-transparent font-black mr-8 focus:outline-none">
+                {i18n.language === 'en' ? 'ID' : 'EN'}
+              </button>
+              <button className="px-6 py-3 bg-[#344e41] text-white rounded-full font-bold text-sm hover:shadow-lg hover:bg-[#3a5a40] focus:outline-none transition-all">
+                <FaPhoneAlt className="inline-block mr-3 text-xs" />
+                {t('contact me')}
               </button>
             </div>
           </div>
         </div>
       </header>
       <section>
-        <div className="flex justify-center items-center mx-auto max-w-[1280px] min-h-[70vh]">
+        <div className="flex justify-center items-center mx-auto max-w-[1280px] py-20">
           <div className="flex-1 max-w-3xl text-center">
-            <div className="relative inline-block bg-white rounded-t-[30px] rounded-bl-[30px] mb-5 px-6 py-4">
-              <span className="font-bold">Hello, My name is Junnaedi</span>
-              {/** make waving animation */}
+            <div className="relative inline-block bg-white shadow-lg rounded-t-[30px] rounded-bl-[30px] mb-5 px-6 py-4">
+              <span className="font-bold">{t('intro', { name: 'Junnaedi' })}</span>
               <AnimatePresence mode='wait'>
                 <motion.div
                   key="wave"
@@ -70,13 +83,60 @@ function App() {
             </div>
             <h1 className="text-5xl leading-tight font-black">
               <span className="bg-clip-text text-transparent bg-gradient-to-r from-[#3a5a40] to-[#6ca376]">
-                Full Stack Developer based in Surabaya, Indonesia
+                {t('heading')}
               </span>
             </h1>
             <p className="mt-5 text-sm leading-loose font-semibold text-gray-800">
-              I would be happy to help you building your next successful product. I have experience working with modern technologies and tools to build scalable and maintainable applications.
+              {t('heading desc')}
             </p>
+            <div className="mt-6 space-x-3">
+              <button className="px-6 py-3 bg-[#344e41] text-white rounded-full font-bold text-md hover:shadow-lg hover:bg-[#3a5a40] focus:outline-none transition-all">
+                <FaChevronDown className="inline-block mr-3 text-sm" />
+                {t('see projects')}
+              </button>
+              <button className="px-6 py-3 bg-[#344e41] text-white rounded-full font-bold text-md hover:shadow-lg hover:bg-[#3a5a40] focus:outline-none transition-all">
+                <FaEnvelope className="inline-block mr-3 text-sm" />
+                {t('contact me')}
+              </button>
+            </div>
           </div>
+        </div>
+      </section>
+      <section className="flex justify-center items-center py-24 mx-auto max-w-[1000px]">
+        <div className="w-7/12">
+          <p className="text-lg leading-loose font-semibold">
+            Berkenalan lebih dekat
+          </p>
+          <h1 className="text-5xl font-[Itim] leading-tight font-black bg-clip-text text-transparent bg-gradient-to-tl from-[#3a5a40] to-[#6ca376]">
+            {t('heading about')}
+          </h1>
+          <p className="mt-5 text-sm leading-loose font-semibold text-gray-800">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus volutpat est sapien, in suscipit nibh condimentum sed. Mauris sed laoreet enim. Nam sed ante ut nulla ultricies consectetur vel a elit.
+          </p>
+          <p className="mt-5 text-sm leading-loose font-semibold text-gray-800">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus volutpat est sapien, in suscipit nibh condimentum sed. Mauris sed laoreet enim. Nam sed ante ut nulla ultricies consectetur vel a elit.
+          </p>
+        </div>
+        <div className="w-5/12 pl-10">
+          <img src={profileImg} alt="profile" className="w-full" />
+        </div>
+      </section>
+      <section className="flex justify-center items-center pb-32 mx-auto max-w-[1000px]">
+        <div className="flex-1">
+          <p className="text-lg leading-loose font-semibold">
+            Alat untuk Development
+          </p>
+          <h3 className="text-5xl font-[Itim] leading-tight font-black bg-clip-text text-transparent bg-gradient-to-tl from-[#3a5a40] to-[#6ca376]">
+              Teknologi yang saya gunakan untuk mengerjakan sebuah project yang valuable
+          </h3>
+        </div>
+        <div className="flex flex-col items-center flex-1">
+          <p className="mt-5 text-sm leading-loose font-semibold text-gray-800">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse aliquam pretium pharetra. Ut placerat bibendum orci, a commodo est molestie vitae. Nam eleifend fermentum sem, ac efficitur eros sagittis ac. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+          </p>
+          <p className="mt-5 text-sm leading-loose font-semibold text-gray-800">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse aliquam pretium pharetra. Ut placerat bibendum orci, a commodo est molestie vitae.
+          </p>
         </div>
       </section>
     </div>
---
+++ src/i18n.ts
@@ -0,0 +1,17 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import en from './locales/en.json';
+import id from './locales/id.json';
+
+i18n.use(initReactI18next).init({
+    debug: true,
+    lng: 'id',
+    resources: {
+      en: {
+        translation: en
+      },
+      id: {
+        translation: id
+      }
+    }
+});
